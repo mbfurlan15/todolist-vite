e8a1fc4e35c84e5da3650600c956a6d6ab159c55
Matheus Furlan <furlan.dmtr@gmail.com>
2021-08-21 07:53:08 -0400

    first

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <h1>{{ titulo }}</h1>
+  <input
+    v-model="tarefa.descricao"
+    type="text"
+    placeholder="Descrição da tarefa"
+  />
+  <!--   <button @click="adicionarTarefa">Adicionar</button>
+ -->
+ <app-btn texto="Adicionar" @click="adicionarTarefa"></app-btn>
+
+  <table border="1">
+    <thead>
+      <th>Concluída</th>
+      <th>Descrição</th>
+    </thead>
+    <tr
+      v-for="iterador in tarefas"
+      :key="iterador"
+      :class="{ riscado: iterador.concluida }"
+    >
+      <td>
+        <input type="checkbox" v-model="iterador.concluida" />
+      </td>
+      <td>{{ iterador.descricao }}</td>
+    </tr>
+  </table>
+</template>
+<script>
+
+import AppBtn from './components/AppBtn.vue'
+
+export default {
+  components: {
+    AppBtn
+  },
+
+  data() {
+    return {
+      titulo: "ToDoList Vite",
+      tarefas: [],
+      tarefa: {
+        descricao: "",
+        concluida: false,
+      },
+    };
+  },
+  methods: {
+    adicionarTarefa() {
+      this.tarefas.push(Object.assign({}, this.tarefa));
+    },
+  },
+};
+</script>
+
+<style>
+.riscado {
+  text-decoration: line-through;
+}
+</style>
---
+++ src/components/AppBtn.vue
@@ -0,0 +1,13 @@
+<template>
+  <button>{{texto}}</button>
+</template>
+
+<script>
+export default {
+    props:["texto"]
+}
+</script>
+
+<style>
+
+</style>
